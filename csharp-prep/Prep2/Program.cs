5c97f627c3b77f37c5c8c327999d45c7a6f0d8b3
bubblehaven <hsoj3sanders@gmail.com>
2025-04-27 00:41:51 -0600

    100pt to letter grade converter completed, including '+' and '-' distinctions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade? (100pt scale) ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80){
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (grade % 10 >= 7 && grade >= 60){
+            letter += "+";
+        }
+        else if (grade % 10 < 3 && grade != 100 && grade >= 60){
+            letter += "-";
+        }
+        Console.WriteLine($"Your letter grade is {letter}.");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed :D");
+        }
+        else
+        {
+            Console.WriteLine("You failed. womp womp. do you know what went wrong? \n let's fix that for next time, you got this!");
+        }
     }
 }
