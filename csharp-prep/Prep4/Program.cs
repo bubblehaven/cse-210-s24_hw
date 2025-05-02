cd0af8d90e37b6220b4f7585388b40245cb28e47
bubblehaven <hsoj3sanders@gmail.com>
2025-05-01 23:53:10 -0600

    list practice completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = 1;
+        List<int> numberlist = [];
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        while (number != 0){
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numberlist.Add(number);
+        }
+        int sum = 0;
+        int highestnum = 0;
+        foreach (int n in numberlist){
+            sum += n;
+            if (n > highestnum){
+                highestnum = n;  
+            }
+        }
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average value was {sum/numberlist.Count}");
+        Console.WriteLine($"The highest number was {highestnum}");
     }
-}
+
+    }
